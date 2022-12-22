ede38770220be7c176ce3a9a0794f2c6965bf6b4
unknown <lakssomi@gmail.com>
2022-12-21 21:35:33 -0800

    javaprograme

--- JavaSprint1/src/test/java/userstory/Collections.java
+++ JavaSprint1/src/test/java/userstory/Collections.java
@@ -1,5 +1,28 @@
 package userstory;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.testng.annotations.Test;
+
 public class Collections {
 
+	public void printvalues() {
+
+		Map<String, List<String>> map = new HashMap<>();
+		List<String> list = new ArrayList<>();
+		map.put("fruit", list);
+		map.get("fruit").add("Apple");
+		map.get("fruit").add("Mango");
+		System.out.println(map);
+
+	}
+
+	@Test
+	public void testcase() {
+		printvalues();
+
+	}
 }
